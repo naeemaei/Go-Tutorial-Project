1fd27a8328579ea436128d1d79e7ef5df9983099
Hamed Naeemaei <naeemaei@outlook.com>
2019-10-20 15:20:55 +0330

    lesson 1 - Hello World!

---
+++ 1- helloworld.go
@@ -0,0 +1,20 @@
+/// Lesson 1 : Hello World!
+
+package main // The package “main” tells the Go compiler that the package should compile as an executable program instead of a shared library
+
+// import necessary libraries
+import (
+	"fmt"
+)
+
+// Golang function
+func hello() {
+
+	fmt.Println("hello World!")
+}
+
+// main function: start of project
+func main() {
+
+	hello()
+}
