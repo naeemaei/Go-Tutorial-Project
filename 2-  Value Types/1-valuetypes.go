89b4181c3000eae707cb107f0f586c69de68695e
Hamed Naeemaei <naeemaei@outlook.com>
2019-10-23 13:22:17 +0330

    bugfix

--- 2-  Value Types/1-valuetypes.go
+++ 2-  Value Types/1-valuetypes.go
@@ -1,11 +1,10 @@
-val.go
 package main
 
 import "fmt"
 
 func main() {
 
-	var counter int = 5
+	var counter int = 5 //sample integer
 
 	var message string // default value ""
 	message = "Hello"
@@ -13,15 +12,19 @@ func main() {
 	var factor float32 // default value 0
 	factor = 4.2
 
+	var firstName, lastName string = "hamed", "naeemaei" // define two variables
+
 	var enabled bool // false
 
-	appName := "Hello Persia"
+	appName := "Hello Persia" //other way to declare variable
 
+	// print data
 	fmt.Println(counter)
 	fmt.Println(message)
 	fmt.Println(factor)
 	fmt.Println(enabled)
 	fmt.Println(appName)
+	fmt.Println(firstName, lastName)
 
 	fmt.Println("7/2 =", 7/2)
 	fmt.Println("7/2.0 =", 7/2.0)
