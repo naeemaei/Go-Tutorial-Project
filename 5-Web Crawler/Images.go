ffef35cff9c484dee1fa24a3a7ed93f36b139b55
Hamed <h.naimaei@gmail.com>
2019-11-03 23:09:41 +0330

    crawl page

--- 5-Web Crawler/Images.go
+++ 5-Web Crawler/Images.go
@@ -10,8 +10,9 @@ import (
 )
 
 func main() {
+
 	// Make HTTP request
-	response, err := http.Get("https://www.varzesh3.com")
+	response, err := http.Get("https://jobinja.ir/jobs?filters%5Bkeywords%5D%5B0%5D=&filters%5Bjob_categories%5D%5B%5D=%D9%88%D8%A8%D8%8C%E2%80%8C+%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%E2%80%8C%D9%86%D9%88%DB%8C%D8%B3%DB%8C+%D9%88+%D9%86%D8%B1%D9%85%E2%80%8C%D8%A7%D9%81%D8%B2%D8%A7%D8%B1&filters%5Blocations%5D%5B%5D=%D8%AA%D9%87%D8%B1%D8%A7%D9%86&sort_by=published_at_desc&page=1")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,17 +25,10 @@ func main() {
 	}
 
 	// Find and print image URLs
-	document.Find("img").Each(func(index int, element *goquery.Selection) {
-		imgSrc, exists := element.Attr("src")
-		if exists {
-			fmt.Println(imgSrc)
-		}
+	document.Find(".o-listView__itemTitle").Each(func(index int, element *goquery.Selection) {
+		class, _ := element.Attr("class")
+
+		fmt.Println(class, element.Text())
 	})
 
-	document.Find("#h3").Each(func(index int, element *goquery.Selection) {
-		Text, exists := element.Attr("class").Value == "m-0 lh-lg"
-		if exists {
-			fmt.Println(Text)
-		}
-	})
 }
