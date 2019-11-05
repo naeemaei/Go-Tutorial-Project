74798a9897598b6ad57ff497c06ad52efd615d3b
Hamed Naeemaei <naeemaei@outlook.com>
2019-11-05 18:28:45 +0330

    read detail page

--- 5-Web Crawler/Images.go
+++ 5-Web Crawler/Images.go
@@ -36,17 +36,26 @@ func main() {
 
 		// Find and print image URLs
 		document.Find(".o-listView__itemInfo").Each(func(index int, element *goquery.Selection) {
-			//class, _ := element.Attr("class")
+
+			// Get company name
 			//var company = element.Find(".c-icon--construction").Parent().Find("span").Text()
 			//fmt.Println(company)
 
+			// Get city name
 			//var place = element.Find(".c-icon--place").Parent().Find("span").Text()
 			//fmt.Println(place)
 
+			// Get job title name
 			//var jobTitle = element.Find(".c-jobListView__titleLink").Text()
 			var jobLink, _ = element.Find(".c-jobListView__titleLink").Attr("href")
-			fmt.Println(jobLink)
-			//fmt.Println(class, element.Text())
+			// fmt.Println(jobLink)
+
+			// Go to detail page
+			var newDocument = MakeHttpRequest(jobLink, 0)
+			var contractType = newDocument.Find(".c-infoBox__itemTitle").Text()
+			fmt.Println(contractType)
+
+			// Save in db
 			// condb := GetConnection()
 			// CreateRecord(condb, jobTitle, company, place)
 
@@ -65,7 +74,10 @@ func MakeHttpRequest(pageAddress string, pageNumber int64) *goquery.Document {
 	// Make HTTP request
 
 	//req, _ := http.NewRequest("GET", "https://jobinja.ir/jobs?filters[job_categories][0]=وب،‌ برنامه‌نویسی و نرم‌افزار&filters[keywords][0]=&filters[locations][0]=تهران&sort_by=relevance_desc&page="+strconv.FormatInt(pageNumber, 10), nil)
-	req, _ := http.NewRequest("GET", pageAddress+strconv.FormatInt(pageNumber, 10), nil)
+	if pageNumber > 0 {
+		pageAddress += strconv.FormatInt(pageNumber, 10)
+	}
+	req, _ := http.NewRequest("GET", pageAddress, nil)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	req.Header.Add("cookie", "__cfduid=d3f05b23a1361bf6bca9e9609ed485b5d1572807113; _ga=GA1.2.925358084.1572807126; _gid=GA1.2.608524599.1572807126; logglytrackingsession=5f9314f7-4f02-4eab-83f1-6cd1c8901c9a; remember_82e5d2c56bdd0811318f0cf078b78bfc=eyJpdiI6Ik96NngyRTV1VlBPMmZjTTFnamZmU3c9PSIsInZhbHVlIjoiOTJMVGdaYTBMODkyUUowTnNWWkxxbks3eCtyQ0pqS0lJUVR5eWlIVit3ekNRSUVuNWNqYzRRaDY2MTNuOHl2KzhERTNrcmVtY2kxc1wvUUxvNERGb1ErZ2t6dVFcL0d2ZVwvRmNwS0o3NThEKzQ9IiwibWFjIjoiNWE0NTU1Nzk2ZDhlNjZhNWY0YzQ5ZDhlNGU4Y2IzYjU3NDVhNzdhYTc4OWMzYTgxMjExYmZmMDQzOWE3YTI2ZiJ9; device_id=eyJpdiI6IldYM253WmkybytwZTB1c0k5ZzYzUHc9PSIsInZhbHVlIjoid2ZwRG1vZFwvMnFmcUF5aTJIcHB1c3c9PSIsIm1hYyI6IjBmYzIzNTgzZjllYjhjOTEyODFmNjY4MjUwZDZjZWUzOTYwYzExMmU4ZGQzMzY4MDc1OTY0NzQ5ZmQ3ODY5YWQifQ%3D%3D; XSRF-TOKEN=eyJpdiI6IkU1dnV3TkJFaWlJeTA0UGtuZUw2UGc9PSIsInZhbHVlIjoid00rSDEyUVZwMG1MSVhOak43cE9jT3NzUXBQblJsdGRaQXFJd0wrb0dyNkNNNkVDU3ZcL1wvQmVvdkpVd256U0RJMzZEbzRwTFNia1NrcENXcEFJZkRxQT09IiwibWFjIjoiY2RmZmMyMTczMmY3ODNkZjIwYzc2ZDlmZTUyODgzZmU1OTAwODk4ZGVjYWViYThjNzMwZjQ4YzY3YjI0YjgwNCJ9; JSESSID=eyJpdiI6IjVtK3JRNjYweEREOEJTenhnc2pxaUE9PSIsInZhbHVlIjoiVU5VUzNTV3V3bmtaT3h6V0dWRE95ck1adFl6T25TdEZBNWZBbkVrSXFpSURRelBORXkyWk9nc0huXC9veE00THUwWCtIeGVPSm1FeDNDc0k4WWhPdXNRPT0iLCJtYWMiOiJjMzQ0ZjI2MjNjZTFhZDI4MDk0Y2I5ZDRhODJkMzk0YTg3YWI4MTg4Y2NmNjhkZjEzYzBkZGUyM2RhZmVjNWI5In0%3D; user_mode=eyJpdiI6IjFGNUtOOTkzeFp2UlJJRnFpV1c4ZEE9PSIsInZhbHVlIjoiZnJxSm14RW4zYkxweTlsRUx2aGFIQT09IiwibWFjIjoiMGExZTc0Nzc5M2JhN2M4MzM4MGU1NDRmY2E0NWRiODczNTgzM2JhNjQ4YTA5MzY2YzA5NTk3NTAwMTg5NmEwZCJ9; _gat_gtag_UA_129125700_1=1")
 	response, err := http.DefaultClient.Do(req)
